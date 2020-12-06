0ac322e819a4f6036a37cd8019b030deb6b4c4fe
Time-litwhite <75021653+Time-litwhite@users.noreply.github.com>
2020-12-06 15:20:26 +0800

    Create xkTool.js

---
+++ xkTool.js
@@ -0,0 +1,8 @@
+var xiaokang = new xkTool();
+xiaokang.appendSocial({
+  "icon-weizhi": "https://baidu.com",
+  "icon-gouwu": "https://a.maorx.cn/",
+  "icon-huiyuan": "https://time-xiaobai.gitee.io/babys-smoke-and-snow/",
+  "icon-youxi": "https://www.google.com/webhp",
+});
+xiaokang.cheatTitle([leaveTitle, backTitle, leaveIcon, backIcon]);
