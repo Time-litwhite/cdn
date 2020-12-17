862c8e89520e505b848a16ef0ca97c61f0451a1d
Time-litwhite <75021653+Time-litwhite@users.noreply.github.com>
2020-12-17 13:58:10 +0800

    Update and rename Video.js to video.js

---
+++ video.js
@@ -0,0 +1,52 @@
+var sUserAgent = navigator.userAgent.toLowerCase();
+var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+var bIsAndroid = sUserAgent.match(/android/i) == "android";
+var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
+    // 判断 header里是否存在full_page
+    if($("#page-header").hasClass("full_page")){
+      // 插入<video>
+      var html = "<video id='video' muted loop autoplay style='height: 100%;width: 100%;object-fit: cover;'></video>";
+      document.getElementsByClassName("full_page")[0].innerHTML += html;
+      // 随机数
+      var randomNum = Math.floor((Math.random() * 15)+1);
+      // 获取 <video>
+      var video = document.getElementById('video');
+      // 插入视频(m3u8)
+      var videoSrc = 'https://cdn.jsdelivr.net/gh/lete114/CDN2/video/'+randomNum+'.m3u8';
+      if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+          video.play();
+        });
+      }
+    }
+}else{
+  // 判断 header里是否存在full_page
+  if($("#page-header").hasClass("full_page")){
+    // 插入<video>
+    var html = "<video id='video' muted loop autoplay style='height: 100%;width: 100%;object-fit: cover;'></video>";
+    document.getElementsByClassName("full_page")[0].innerHTML += html;
+    // 随机数
+    var randomNum = Math.floor((Math.random() * 6)+1);
+    // 获取 <video>
+    var video = document.getElementById('video');
+    // 插入视频(m3u8)
+    var videoSrc = 'https://cdn.jsdelivr.net/gh/lete114/CDN2/video/mobile/'+randomNum+'.m3u8';
+    if (Hls.isSupported()) {
+      var hls = new Hls();
+      hls.loadSource(videoSrc);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        video.play();
+      });
+    }
+  }
+}
