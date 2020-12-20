45485c1cb727a8652fd48f512df384dfc7cd7084
Time-litwhite <75021653+Time-litwhite@users.noreply.github.com>
2020-12-20 18:36:21 +0800

    Update and rename autols.js to autol.js

--- live2d/autol.js
+++ live2d/autol.js
@@ -34,8 +34,8 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "/Time-litwhite.github.io/live2d_api/",
-		        //cdnPath: "https://cdn.jsdelivr.net/gh/Time-litwhite/cdn/live2d_api/"
+			//apiPath: "/Time-litwhite.github.io/live2d_api/",
+		        cdnPath: "https://live2d-1304492190.cos.ap-nanjing.myqcloud.com/live_api/"
 		});
 	});
 console.log(`
